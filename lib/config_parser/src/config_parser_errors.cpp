43deb69ff3f7dc545c01ae41b9d44b6b167fc075
ptrvsrg <s.petrov1@g.nsu.ru>
2022-11-18 17:14:57 +0700

    Add error description

--- lib/config_parser/src/config_parser_errors.cpp
+++ lib/config_parser/src/config_parser_errors.cpp
@@ -1,4 +1,4 @@
 #include "config_parser_errors.h"
 
-OpeningException::OpeningException(const std::string &file_name)
-    : logic_error(file_name) {}
+OpeningException::OpeningException(const std::string & file_path)
+    : logic_error(file_path + ": File opening error") {}
