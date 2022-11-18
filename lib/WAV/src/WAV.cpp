2d5e8bb614b3b875c91e82bb142237c38eaa13db
ptrvsrg <s.petrov1@g.nsu.ru>
2022-11-18 17:15:19 +0700

    Update WAV module

--- lib/WAV/src/WAV.cpp
+++ lib/WAV/src/WAV.cpp
@@ -1,24 +1,9 @@
 #include "WAV.h"
-#include "WAV_errors.h"
 
-WAV::WAV(const std::string & file_name,
-         std::ios_base::openmode mode,
-         FMTData fmt_data)
-:   file_name_(file_name),
-    mode_(mode),
-    fmt_data_(fmt_data)
-{
-    if (file_name_.find(".wav") == std::string::npos) throw ExtensionException(file_name_);
-    stream_.open(file_name,
-                 std::ios_base::binary | mode);
-    if (!stream_.good()) throw OpeningException(file_name_);
-
-    if (mode_ & std::ios_base::in)  ReadHeader();
-    if (mode_ & std::ios_base::out) WriteHeader(fmt_data_);
-}
+WAV::WAV(std::string file_path)
+:   file_path_(std::move(file_path)) {}
 
 WAV::~WAV()
 {
-    if (mode_ & std::ios_base::out) FixHeader();
-    stream_.close();
+    if (!stream_.is_open()) stream_.close();
 }
