c2ccd4bfac09b4e5197fe573021c9787af73312b
ptrvsrg <s.petrov1@g.nsu.ru>
2022-11-19 23:10:37 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "cl_parser.h"
+#include "sound_processor.h"
 
 int main(int argc,
          char ** argv)
@@ -11,10 +12,16 @@ int main(int argc,
         GetOptions(argc,
                    argv,
                    opts);
+
+        SoundProcessor sound_processor(opts.config_file_,
+                                       opts.output_file_,
+                                       opts.input_files_);
+        sound_processor.Convert();
     }
     catch (const std::exception & ex)
     {
         std::cerr << ex.what();
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
