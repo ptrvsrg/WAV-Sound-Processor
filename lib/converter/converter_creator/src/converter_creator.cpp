d366b56e463c8bc23d3e1d09953246cbdad2c90e
ptrvsrg <s.petrov1@g.nsu.ru>
2022-11-22 14:53:30 +0700

    Refactoring

--- lib/converter/converter_creator/src/converter_creator.cpp
+++ lib/converter/converter_creator/src/converter_creator.cpp
@@ -6,7 +6,10 @@ ConverterPtr ConverterCreator::Create(ConverterCommand converter_command)
     std::string converter_name = converter_command[0];
     converter_command.erase(converter_command.cbegin());
 
-    if (converter_name == "mute")       return std::make_unique<MuteConverter>(converter_command);
-    else if (converter_name == "mix")   return std::make_unique<MixConverter>(converter_command);
-    else                                throw IncorrectConverterNameException(converter_name);
+    if (converter_name == "mute")
+        return std::make_unique<MuteConverter>(converter_command);
+    else if (converter_name == "mix")
+        return std::make_unique<MixConverter>(converter_command);
+    else
+        throw IncorrectConverterNameException(converter_name);
 }
